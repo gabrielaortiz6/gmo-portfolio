426324c9e23bc42716f7e557e880b4b42635d23b
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-07-12 13:19:17 -0400

    added file structure

---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+
+const Header = () => {
+  return (
+    <header>
+      {/* Developer's name */}
+      {/* Navigation component */}
+    </header>
+  );
+};
+
+export default Header;
+
