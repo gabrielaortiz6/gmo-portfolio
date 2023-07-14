e696da1323df4fa459d5a079d2e3c6a831e48788
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-07-14 19:59:34 -0400

    added font

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Helmet } from 'react-helmet';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Helmet>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap" rel="stylesheet" />
+    </Helmet>
     <App />
   </React.StrictMode>,
     document.getElementById('root')
